e900f03b1072ad2347bcb1eb8304c6f00bcdef52
Aldair Leon <Aldair.Leon@blueyonder.com>
2021-10-26 16:29:45 -0500

    git ignore change

--- scripts/main.py
+++ scripts/main.py
@@ -2,8 +2,6 @@ from report_creation import IngestionReport
 """
 Author: Aldair Leon
 Date: Oct 26th, 2021
-
-This code convert SQL data into CSV file from SW Connect DB
 """
 
 # This class request of date_start and date_finish in this format YY/MM/DD HH:MM:SS.FFF and
@@ -13,6 +11,6 @@ This code convert SQL data into CSV file from SW Connect DB
 date_start = '21/10/25 11:00:00.000'
 date_finish = '21/10/25 12:00:00.000'
 env = 'sql_db_prod'
-x = IngestionReport(date_start, date_finish, env)
-x.detail_report()
-x.summary_report()
+report = IngestionReport(date_start, date_finish, env)
+report.detail_report()
+report.summary_report()
